6d30de56c8ea6878e0132afdd8bcdb0f2654c2a6
Arnold <arnold.muller.external@prisma.movie>
2021-12-11 19:00:02 +0100

    feat: implemented new amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "shelfui": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
